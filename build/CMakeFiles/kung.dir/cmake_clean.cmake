cac80ba3a88f252f56ba1831f63fcb1c6400da88
Elias Rosberg <elias.rosberg@hotmail.com>
2024-01-27 12:21:15 +0100

    setup cmake and project

---
+++ build/CMakeFiles/kung.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kung.dir/src/main.cpp.o"
+  "CMakeFiles/kung.dir/src/main.cpp.o.d"
+  "kung"
+  "kung.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kung.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
